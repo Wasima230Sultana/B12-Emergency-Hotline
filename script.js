1fd8bb80d040aa55cdc90d1bbce26330524d29ef
wasimasultana230 <wasimasultana230@gmail.com>
2025-08-29 19:45:59 +0600

    Add police helpline number

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function setCoin(value) {
 function showAlert(id1, id2) {
     const serviceName = document.getElementById(id1).innerText;
     const serviceNumber = document.getElementById(id2).innerText;
-    return alert(serviceName + ' and ' + serviceNumber);
+    return alert('Calling '+serviceName + ' ' + serviceNumber+'...');
 }
 
 
@@ -94,3 +94,43 @@ document.getElementById("call-press").addEventListener('click', function () {
 
 
 // Police Helpline Number
+
+
+// copy button 
+document.getElementById('police-helpline-number-copy').addEventListener('click', function () {
+    countCopyBtn('copy-count', 'police-helpline-number');
+})
+
+
+// coin button and call button
+document.getElementById("police-helpline-number-call").addEventListener('click', function () {
+    const total = initialCoin("coin-count");
+    if (total < 20) {
+        alert('Not sufficient coin');
+        return;
+    }
+    const remainingCoin = total - 20;
+    setCoin(remainingCoin);
+    showAlert('police-helpline', 'police-helpline-number');
+    const data = {
+        name: 'Police Helpline Number',
+        number: '999',
+        time: new Date().toLocaleTimeString('en-US', { hour12: true })
+    }
+    history.push(data);
+    const callHistory = document.getElementById('call-history-container');
+    callHistory.innerText = "";
+    for (const data of history) {
+        const div = document.createElement("div");
+        div.innerHTML = `
+    <div class="flex justify-between items-center p-5 mt-4 bg-[#FAFAFA] rounded-xl">
+                    <div>
+                    <h1 class="font-semibold text-lg">${data.name}</h1>
+                    <p class="text-[#5C5C5C]">${data.number}</p>
+                    </div>
+                    <h1 class="text-[#111111]">${data.time}</h1>
+                </div>
+    `
+        callHistory.appendChild(div);
+    }
+})
